69c20b277adaea891017f22b6f144b6cab3c4035
Phucleee <lehuuphuc.work@gmail.com>
2025-11-26 10:08:41 +0700

    add language greett

--- app.py
+++ app.py
@@ -1,7 +1,14 @@
-def greet(name):
-    print(f"Hello, {name}!")
-    print("This is version 2 of our app.")
-    print("Have a nice day!")
+def greet(name, language="en"):
+    if language == "en":
+        print(f"Hello, {name}!")
+        print("This is version 2 of our app.")
+        print("Have a nice day!")
+    elif language == "vi":
+        print(f"Xin chào, {name}!")
+        print("Đây là phiên bản 2 của ứng dụng.")
+        print("Chúc bạn một ngày tốt lành!")
+    else:
+        print(f"Hello, {name} (language not supported).")
 
 if __name__ == "__main__":
-    greet("Phuc")
+    greet("Phuc", "vi")
