8897ba40f7c28bbe68ea96e81b307a3335461dc0
Phucleee <lehuuphuc.work@gmail.com>
2025-11-26 10:04:07 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 def greet(name):
     print(f"Hello, {name}!")
-    print("This is version 1 of our app.")
+    print("This is version 2 of our app.")
+    print("Have a nice day!")
 
 if __name__ == "__main__":
     greet("Phuc")
